9b914df9b81d2a94135f6b9cc35224ae9e2bd6d6
Berkay  Kaya <mberkaykaya@outlook.com>
2022-01-19 19:29:25 +0300

    uielements update

--- SampleExample/ViewController.swift
+++ SampleExample/ViewController.swift
@@ -14,16 +14,26 @@ class ViewController: UIViewController {
     private let mainStackView = UIStackViewBuilder()
         .axis(.vertical)
         .spacing(20)
+        .alignment(.center)
+        .build()
+    private let buttonStackView = UIStackViewBuilder()
+        .axis(.horizontal)
+        .alignment(.center)
+        .spacing(50)
         .build()
     private let firstTextField = UITextFieldBuilder()
         .borderWidth(0.5)
-        .placeholder("Please enter your first number.")
+        .placeholder("Please enter your number.")
         .build()
     private let secondTextField = UITextFieldBuilder()
         .borderWidth(0.5)
-        .placeholder("Please enter your second number.")
+        .placeholder("Please enter your number.")
         .build()
-    private let sumButton = UIButtonBuilder().title("+").build()
+    private let resultLabel = UILabelBuilder().text("Result:").build()
+    private let sumButton = UIButtonBuilder().title("+").backgroundColor(.blue).build()
+    private let difButton = UIButtonBuilder().title("-").backgroundColor(.brown).build()
+    private let multiplyButton = UIButtonBuilder().title("*").backgroundColor(.cyan).build()
+    private let divideButton = UIButtonBuilder().title("/").backgroundColor(.green).build()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,15 +46,28 @@ class ViewController: UIViewController {
 extension ViewController {
     private func addSubViews() {
         addMainStackView()
+        addElementsOfStackView()
+        addButtonsStackView()
     }
     
     private func addMainStackView() {
         view.addSubview(mainStackView)
         mainStackView.topToSuperview(usingSafeArea: true).constant = 20
-        mainStackView.trailingToSuperview().constant = 30
-        mainStackView.bottomToSuperview(usingSafeArea: true).constant = -50
-        mainStackView.leadingToSuperview().constant = -30
-        mainStackView.backgroundColor = .red
-        
+        mainStackView.trailingToSuperview().constant = -30
+        mainStackView.leadingToSuperview().constant = 30
+    }
+    
+    private func addElementsOfStackView() {
+        mainStackView.addArrangedSubview(firstTextField)
+        mainStackView.addArrangedSubview(secondTextField)
+        mainStackView.addArrangedSubview(buttonStackView)
+        mainStackView.addArrangedSubview(resultLabel)
+    }
+    
+    private func addButtonsStackView() {
+        buttonStackView.addArrangedSubview(sumButton)
+        buttonStackView.addArrangedSubview(multiplyButton)
+        buttonStackView.addArrangedSubview(difButton)
+        buttonStackView.addArrangedSubview(divideButton)
     }
 }
