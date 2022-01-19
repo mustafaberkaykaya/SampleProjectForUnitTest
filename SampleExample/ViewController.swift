71585a8dee8575ddb6b5acdc93ce1fca7485b1c5
Berkay  Kaya <mberkaykaya@outlook.com>
2022-01-19 19:02:15 +0300

    add mainStackView

--- SampleExample/ViewController.swift
+++ SampleExample/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import MobilliumBuilders
+import TinyConstraints
 
 class ViewController: UIViewController {
+    
+    private let mainStackView = UIStackViewBuilder()
+        .axis(.vertical)
+        .spacing(20)
+        .build()
+    private let firstTextField = UITextFieldBuilder()
+        .borderWidth(0.5)
+        .placeholder("Please enter your first number.")
+        .build()
+    private let secondTextField = UITextFieldBuilder()
+        .borderWidth(0.5)
+        .placeholder("Please enter your second number.")
+        .build()
+    private let sumButton = UIButtonBuilder().title("+").build()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addSubViews()
     }
 
+}
+
+extension ViewController {
+    private func addSubViews() {
+        addMainStackView()
+    }
+    
+    private func addMainStackView() {
+        view.addSubview(mainStackView)
+        mainStackView.topToSuperview(usingSafeArea: true).constant = 20
+        mainStackView.trailingToSuperview().constant = 30
+        mainStackView.bottomToSuperview(usingSafeArea: true).constant = -50
+        mainStackView.leadingToSuperview().constant = -30
+        mainStackView.backgroundColor = .red
+        
+    }
 }
